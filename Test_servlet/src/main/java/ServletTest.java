43fc27fffb198475d5ce65d76860b458595e81bf
AnatolSich <tip-77@ukr.net>
2017-12-19 16:20:52 +0200

    Deleted error in Servlet

--- Test_servlet/src/main/java/ServletTest.java
+++ Test_servlet/src/main/java/ServletTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class ServletTest extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       req.getRequestDispatcher("helloJsp.jsp").forward(req,resp);
+        req.getRequestDispatcher("helloJSP.jsp").forward(req,resp);
     }
 
     @Override
